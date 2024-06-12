ab273b49765f921ec31a216f0a0f28ac8e2a4fad
MatKarnicki <matkarnicki@gmail.com>
2024-06-13 00:32:41 +0200

    Genetic algorithm implemented SUCCESFULLY!

--- code/Crypt_genetic.py
+++ code/Crypt_genetic.py
@@ -1,15 +1,41 @@
 import pygame
 import sys
 import random
+import numpy as np
 
 points = 0
 amount_of_moves = 0
 
+
+# Screen dimensions
+WIDTH, HEIGHT = 1000, 1000
+GRID_SIZE = 100
+ROWS = 10
+COLS = 10
+# screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# pygame.display.set_caption("Crypt of the AIdancer")
+
+# Define the grid
+EMPTY = 0
+PLAYER = 1
+WALL = 2
+BAT = 3
+BAT_ATTACKING = 4
+SLIME = 5
+SLIME_ATTACKING = 6
+SKELETON = 7
+SKELETON_ATTACKING = 8
+
+background = pygame.transform.scale(pygame.image.load("../assets/sprites/floor.png"), (GRID_SIZE, GRID_SIZE))
+background_lite = pygame.transform.scale(pygame.image.load("../assets/sprites/floor2.png"), (GRID_SIZE, GRID_SIZE))
+grid = [[EMPTY for _ in range(COLS)] for _ in range(ROWS)]
+
+
 class Player:
     def __init__(self, grid_pos, health):
         self.grid_pos = grid_pos
-        self.image_idle = pygame.transform.scale(pygame.image.load("assets/sprites/player.png"), (GRID_SIZE, GRID_SIZE))
-        self.image_hurt = pygame.transform.scale(pygame.image.load("assets/sprites/player_hurt.png"),
+        self.image_idle = pygame.transform.scale(pygame.image.load("../assets/sprites/player.png"), (GRID_SIZE, GRID_SIZE))
+        self.image_hurt = pygame.transform.scale(pygame.image.load("../assets/sprites/player_hurt.png"),
                                                  (GRID_SIZE, GRID_SIZE))
         self.image = self.image_idle
         self.health = health
@@ -51,7 +77,6 @@ class Player:
             if self.health <= 0:
                 grid[self.grid_pos[1]][self.grid_pos[0]] = EMPTY
                 self.alive = False
-            print(self.health)
             self.iframes = True
 
 
@@ -82,9 +107,10 @@ class Enemy:
             points += 100
 
 
+
 class Bat(Enemy):
     def __init__(self, grid_pos, health):
-        super().__init__("assets/sprites/bat_idle.png", "assets/sprites/bat_attack.png", grid_pos, health)
+        super().__init__("../assets/sprites/bat_idle.png", "../assets/sprites/bat_attack.png", grid_pos, health)
         grid[self.grid_pos[1]][self.grid_pos[0]] = BAT
 
     def move(self, player):
@@ -114,7 +140,7 @@ class Bat(Enemy):
 
 class Slime(Enemy):
     def __init__(self, grid_pos, health):
-        super().__init__("assets/sprites/slime_idle.png", "assets/sprites/slime_attack.png", grid_pos, health)
+        super().__init__("../assets/sprites/slime_idle.png", "../assets/sprites/slime_attack.png", grid_pos, health)
         grid[self.grid_pos[1]][self.grid_pos[0]] = SLIME
         self.up = True
 
@@ -146,7 +172,7 @@ class Slime(Enemy):
 
 class DestructibleWall(Enemy):
     def __init__(self, grid_pos):
-        super().__init__("assets/sprites/wall.png", "assets/sprites/wall.png", grid_pos, 1)
+        super().__init__("../assets/sprites/wall.png", "../assets/sprites/wall.png", grid_pos, 1)
         grid[self.grid_pos[1]][self.grid_pos[0]] = WALL
 
     def take_dmg(self, amount):
@@ -159,7 +185,8 @@ class DestructibleWall(Enemy):
 
 class Skeleton(Enemy):
     def __init__(self, grid_pos, health, direction):
-        super().__init__("assets/sprites/skeleton_idle.png", "assets/sprites/skeleton_attack.png", grid_pos, health)
+        super().__init__("../assets/sprites/skeleton_idle.png", "../assets/sprites/skeleton_attack.png", grid_pos,
+                         health)
         self.direction = direction  # true - vertical false - "horizontal"
 
     def move(self, player):
@@ -196,50 +223,6 @@ class Skeleton(Enemy):
             self.image = self.image_attack
 
 
-def isAlive(enemy):
-    return enemy.alive
-
-
-def get_entity_at_pos(pos, entities):
-    for entity in entities:
-        if entity.grid_pos == pos:
-            return entity
-    return None
-
-
-# Initialize Pygame
-pygame.init()
-
-# Screen dimensions
-WIDTH, HEIGHT = 1000, 1000
-GRID_SIZE = 100
-ROWS = 10
-COLS = 10
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Crypt of the AIdancer")
-
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-
-# Define the grid
-EMPTY = 0
-PLAYER = 1
-WALL = 2
-BAT = 3
-BAT_ATTACKING = 4
-SLIME = 5
-SLIME_ATTACKING = 6
-SKELETON = 7
-SKELETON_ATTACKING = 8
-
-background = pygame.transform.scale(pygame.image.load("assets/sprites/floor.png"), (GRID_SIZE, GRID_SIZE))
-background_lite = pygame.transform.scale(pygame.image.load("assets/sprites/floor2.png"), (GRID_SIZE, GRID_SIZE))
-grid = [[EMPTY for _ in range(COLS)] for _ in range(ROWS)]
-
 def create_entities(matrix):
     player = []
     enemies = []
@@ -251,7 +234,7 @@ def create_entities(matrix):
             elif value == SLIME:
                 enemies.append(Slime([x, y], 2))
             elif value == SKELETON:
-                enemies.append(Skeleton([x, y], 1, random.choice([True, False])))
+                enemies.append(Skeleton([x, y], 1, False))
             elif value == BAT:
                 enemies.append(Bat([x, y], 1))
             elif value == WALL:
@@ -259,14 +242,28 @@ def create_entities(matrix):
     return player, enemies
 
 
+def isAlive(enemy):
+    return enemy.alive
+
+
+def get_entity_at_pos(pos, entities):
+    for entity in entities:
+        if entity.grid_pos == pos:
+            return entity
+    return None
+
+
 def isNotWall(enemy):
     return not isinstance(enemy, DestructibleWall)
-# PLAYER = 1
-# WALL = 2
-# BAT = 3
-# SLIME = 5
-# SKELETON = 7
-def main():
+
+
+# Genetic Algorithm functions
+def initialize_population(size, genome_length):
+    return [np.random.choice(['LEFT', 'RIGHT', 'UP', 'DOWN'], genome_length) for _ in range(size)]
+
+
+def evaluate_genome(genome):
+    global grid, points, amount_of_moves
     matrix = [
         [2, 2, 2, 1, 2, 2, 2, 2, 2, 2],
         [2, 2, 2, 0, 2, 2, 2, 2, 2, 2],
@@ -279,51 +276,75 @@ def main():
         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     ]
+    grid = [[EMPTY for _ in range(COLS)] for _ in range(ROWS)]
+    points = 0
+    amount_of_moves = 0
     player, enemies = create_entities(matrix)
-    prev_turn_iframes = False
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    player[0].move("LEFT", enemies)
-                elif event.key == pygame.K_RIGHT:
-                    player[0].move("RIGHT", enemies)
-                elif event.key == pygame.K_UP:
-                    player[0].move("UP", enemies)
-                elif event.key == pygame.K_DOWN:
-                    player[0].move("DOWN", enemies)
-                enemies = list(filter(isAlive, enemies))
-                for enemy in enemies:
-                    enemy.move(player)
-                if player[0].iframes and not prev_turn_iframes:
-                    prev_turn_iframes = True
-                else:
-                    player[0].iframes = False
-                    prev_turn_iframes = False
+    for move in genome:
         if not player[0].alive:
-            running = False
-
-        # Draw everything
-        for y in range(0, ROWS):
-            for x in range(0, COLS):
-                if (x + y) % 2 == 0:
-                    screen.blit(background, (x * GRID_SIZE, y * GRID_SIZE))
-                else:
-                    screen.blit(background_lite, (x * GRID_SIZE, y * GRID_SIZE))
-        player[0].draw(screen)
+            break
+        player[0].move(move, enemies)
+        enemies = list(filter(isAlive, enemies))
         for enemy in enemies:
-            enemy.draw(screen)
+            enemy.move(player)
+        enemies_without_walls = list(filter(isNotWall, enemies))
+        if len(enemies_without_walls) == 0:
+            break
+    return points + player[0].health * 500 - len(enemies_without_walls) * 1000 - amount_of_moves * 100
 
-        pygame.display.flip()
-    global points
-    enemies_without_walls = list(filter(isNotWall, enemies))
-    print(points + player[0].health * 500 + len(enemies_without_walls) * -250 + amount_of_moves * -100)
-    pygame.quit()
-    sys.exit()
 
+def selection(population, fitnesses, num_parents):
+    selected_indices = np.argsort(fitnesses)[-num_parents:]
+    return [population[i] for i in selected_indices]
+
+
+def crossover(parent1, parent2):
+    point = random.randint(1, len(parent1) - 1)
+    child1 = np.concatenate([parent1[:point], parent2[point:]])
+    child2 = np.concatenate([parent2[:point], parent1[point:]])
+    return child1, child2
+
+
+def mutate(genome, mutation_rate=0.01):
+    for i in range(len(genome)):
+        if random.random() < mutation_rate:
+            genome[i] = random.choice(['LEFT', 'RIGHT', 'UP', 'DOWN'])
+    return genome
+
+
+def genetic_algorithm():
+    population_size = 300
+    genome_length = 30
+    num_generations = 50
+    num_parents = 30
+    mutation_rate = 0.2
+
+    population = initialize_population(population_size, genome_length)
+
+    for generation in range(num_generations):
+        fitnesses = [evaluate_genome(genome) for genome in population]
+        parents = selection(population, fitnesses, num_parents)
+        next_population = []
+        for i in range(population_size // 2):
+            parent1, parent2 = random.sample(parents, 2)
+            child1, child2 = crossover(parent1, parent2)
+            next_population.append(mutate(child1, mutation_rate))
+            next_population.append(mutate(child2, mutation_rate))
+        population = next_population
+        fitnesses = [evaluate_genome(genome) for genome in population]
+        best_fitness = max(fitnesses)
+        print(f'Generation {generation + 1}, Best Fitness: {best_fitness}')
+        best_genome = population[np.argmax(fitnesses)]
+        if best_fitness == 2000:
+            print(population)
+            break
+    return best_genome
+
+
+
+def main():
+    best_genome = genetic_algorithm()
+    print(f'Best Genome: {best_genome}')
 
 if __name__ == "__main__":
     main()
